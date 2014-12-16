910b19e08697189b4236e99a1a4c345bbd054459
Todd Channick <toc5012@gmail.com>
2014-12-15 20:58:55 -0500

    minor js changes

--- js/app.js
+++ js/app.js
@@ -1,24 +1,29 @@
-
 $(document).ready(function() {
 	var results = $('.results');	
 	$('.start').click(function(){
 		var userInput = $('#usernumber').val();
-		for (i=userInput; i<=100; i++){
-			if (i%5 == 0 && i%3 == 0) {
-				results.append('<li>FizzBuzz</li>')
-			}
-			else if (i%3 == 0) {
-				results.append('<li>Fizz</li>')
-			}
-			else if (i%5 == 0) {
-				results.append('<li>Buzz</li>')
-			}
-			
-			else {
-				results.append('<li>'+i+'</li>')
-			}
+		if (userInput%1 != 0){
+			alert('Please enter an integer value. No decimals.')
+			$('#usernumber').val('');
+		}
+		else {
+			for (i=userInput; i<=100; i++){
+				if (i%5 == 0 && i%3 == 0) {
+					results.append('<li>FizzBuzz</li>')
+				}
+				else if (i%3 == 0) {
+					results.append('<li>Fizz</li>')
+				}
+				else if (i%5 == 0) {
+					results.append('<li>Buzz</li>')
+				}
+				else {
+					results.append('<li>'+i+'</li>')
+				};
+			};
 		};
 	});
 });
+
 
 
